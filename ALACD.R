91bdb65ef12b7871be1b9f9de4f386dd248af5fd
wangyc82 <36029032+wangyc82@users.noreply.github.com>
2019-04-16 10:59:49 +0800

    Delete ALACD.R

--- ALACD.R
+++
@@ -1,7 +0,0 @@
-# this is actuaaly the read me file, including the annotation of the listing R code files
-# crossvalSVM.R is for SVM cross-validtion
-# getperf.R is for generating the evluation criteria
-# enhancering-lncRNA-mRNA-coexpression.R is ro get enhancered lncRN-mRNA co-expression relationships via SVM
-# optimizating.R is toe get optimized overlap lncRNA aassociated genes and drug induced gene signatures, and then to get the lncRNA-drug association
-# DEGanalysis.R is to perform the differential expression analysis
-$ survival-analysis.R is to perform surivival analysis of lncRNAs
